66cbfbdeadc24e0c927ad3b4863a0c4ddc537632
zentred <87871486+zentred@users.noreply.github.com>
2022-01-08 06:05:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,26 +9,29 @@ os.system('cls')
 with open("config.json", "r") as config:
     config = json.load(config)
 
-webhook = config['webhook']
-cookie = config['cookie']
-use_minimum = config['use_minimum_items']
-my_minimum = config['minimum_items_my_side']
-their_minimum = config['minimum_items_their_side']
-decline_projected = config['decline_receive_projected']
-keep_giving_projected = config['keep_giving_projected']
-blacklisted_traders = config['blacklisted_traders']
-blacklisted_giving = config['blacklisted_giving']
-blacklisted_receiving = config['blacklisted_receiving']
-trade_mode = config['trade_type']
+webhook = config['information']['webhook']
+cookie = config['information']['cookie']
+minimum_rap_for_webhook = config['information']['minimum_win_for_webhook']
+
+trade_mode = config['trade_information']['type']
+sort_by = config['trade_information']['sort']
+
+my_minimum = config['trade_information']['my_offer']['minimum_items']
+their_minimum = config['trade_information']['their_offer']['minimum_items']
+decline_projected = config['trade_information']['my_offer']['decline_projected']
+keep_giving_projected = config['trade_information']['their_offer']['decline_projected']
+
+blacklisted_traders = config['trade_information']['blacklisted_users']
+blacklisted_giving = config['trade_information']['my_offer']['blacklisted_items']
+blacklisted_receiving = config['trade_information']['their_offer']['blacklisted_items']
+
 req.cookies['.ROBLOSECURITY'] = cookie
 
-trades = 0
-good = 0
-bad = 0
+trades, good, bad, skipped = 0, 0, 0, 0
 
 def title():
     while True:
-        ctypes.windll.kernel32.SetConsoleTitleW(f'Trades Inbound: {trades} | Good Trades: {good} | Declined Trades: {bad}')
+        ctypes.windll.kernel32.SetConsoleTitleW(f'Total Trades: {trades} | Good Trades: {good} | Declined Trades: {bad} | Trades Already Checked: {skipped}')
 
 def rolimons():
     ids = []
@@ -61,13 +64,11 @@ def scrape_trades():
                 cursor = r['nextPageCursor']
                 if not cursor == None:
                     for i in range(len(r['data'])):
-                        id = r['data'][i]['id']
-                        tradeids.append(id)
+                        tradeids.append(r['data'][i]['id'])
                         trades += 1
                 elif cursor == None:
                     for i in range(len(r['data'])):
-                        id = r['data'][i]['id']
-                        tradeids.append(id)
+                        tradeids.append(r['data'][i]['id'])
                         trades += 1
                     print(f'{Fore.LIGHTGREEN_EX}[+] Finished Scraping - {len(tradeids)} Trades Found')
                     return tradeids
@@ -80,117 +81,119 @@ def scrape_trades():
             continue
 
 def check(inbounds):
-    global good, bad, blacklisted
+    global good, bad, blacklisted, skipped
     for trade in inbounds:
-        dont = open('dontsend.txt','r').read()
+        dont = open('checked.txt','r').read()
         try:
-            me = 0
-            them = 0
-            decline = False
-            me_proj = False
+            me, them, decline, me_proj = 0, 0, False, False
             while True:
                 r = req.get(f'https://trades.roblox.com/v1/trades/{trade}').json()
                 if 'userAssets' in str(r):
                     trade_identifier = r['id']
-                    me_hook = []
-                    them_hook = []
-                    themvalues_hook = []
-                    mevalues_hook = []
-                    uaid = str(r['offers'][1]['user']['id'])
+                    if not f'{trade_identifier}' in dont:
+                        me_hook = []
+                        them_hook = []
+                        themvalues_hook = []
+                        mevalues_hook = []
+                        uaid = str(r['offers'][1]['user']['id'])
 
-                    for i in range(len(r['offers'][0]['userAssets'])):
-                        id = str(r['offers'][0]['userAssets'][i]['assetId'])
-                        current, name, proj = values[id].split('/',3)
-                        me += int(current)
-                        crn = "{:,}".format(int(current))
-                        me_hook.append(f'**Item**: {name}\n**Value**: {crn}\n')
-                        if int(id) in blacklisted_giving:
-                            decline = True
-                        if keep_giving_projected == False:
-                            if str(proj) == '1':
-                                me_proj = True
-                    myrobux = r['offers'][0]['robux']
-                    me_hook.append(f'**Robux**: {myrobux}\n')
-
-                    for i in range(len(r['offers'][1]['userAssets'])):
-                        id = str(r['offers'][1]['userAssets'][i]['assetId'])
-                        current, name, proj = values[id].split('/',3)
-                        them += int(current)
-                        crn = "{:,}".format(int(current))
-                        them_hook.append(f'**Item**: {name}\n**Value**: {crn}\n')
-                        if int(id) in blacklisted_receiving:
-                            decline = True
-                        if decline_projected == True:
-                            if str(proj) == '1':
+                        for i in range(len(r['offers'][0]['userAssets'])):
+                            id = str(r['offers'][0]['userAssets'][i]['assetId'])
+                            current, name, proj = values[id].split('/',3)
+                            me += int(current)
+                            crn = "{:,}".format(int(current))
+                            me_hook.append(f'**Item**: {name}\n**Value**: {crn}\n')
+                            if int(id) in blacklisted_giving:
                                 decline = True
-                    theirrobux = r['offers'][1]['robux']
-                    them_hook.append(f'**Robux**: {theirrobux}\n')
+                            if keep_giving_projected == False:
+                                if str(proj) == '1':
+                                    me_proj = True
+                        myrobux = r['offers'][0]['robux']
+                        me_hook.append(f'**Robux**: {myrobux}\n')
 
-                    if use_minimum == True:
+                        for i in range(len(r['offers'][1]['userAssets'])):
+                            id = str(r['offers'][1]['userAssets'][i]['assetId'])
+                            current, name, proj = values[id].split('/',3)
+                            them += int(current)
+                            crn = "{:,}".format(int(current))
+                            them_hook.append(f'**Item**: {name}\n**Value**: {crn}\n')
+                            if int(id) in blacklisted_receiving:
+                                decline = True
+                            if decline_projected == True:
+                                if str(proj) == '1':
+                                    decline = True
+                        theirrobux = r['offers'][1]['robux']
+                        them_hook.append(f'**Robux**: {theirrobux}\n')
+
                         if len(r['offers'][1]['userAssets']) > their_minimum:
                             decline = True
                         if len(r['offers'][0]['userAssets']) > their_minimum:
                             decline = True
-                    if uaid in blacklisted_traders:
-                        decline = True
-                    if keep_giving_projected == True:
-                        if me >= them and me_proj == False:
+                        if uaid in blacklisted_traders:
                             decline = True
-                    elif keep_giving_projected == False:
-                        if me >= them:
-                            decline = True
+                        if keep_giving_projected == True:
+                            if me >= them and me_proj == False:
+                                decline = True
+                        elif keep_giving_projected == False:
+                            if me >= them:
+                                decline = True
 
-                    if decline == True:
-                        while True:
-                            r = req.post('https://auth.roblox.com/v1/logout')
-                            csrf = r.headers['X-CSRF-TOKEN']
-                            headers = {
-                                'x-csrf-token': csrf
-                                }
-                            a = req.post(f'https://trades.roblox.com/v1/trades/{trade}/decline', headers=headers).text
-                            if a == '{}':
-                                print(f'{Fore.WHITE}[{Fore.RED}x{Fore.WHITE}] {Fore.RED}Trade Declined {Fore.WHITE}- {Fore.RED}{me} {Fore.WHITE}for {Fore.RED}{them}')
-                                bad += 1
-                                break
-                            elif 'Declined' in a:
-                                break
-                            else:
-                                print(f'{Fore.WHITE}[{Fore.YELLOW}-{Fore.WHITE}]{Fore.YELLOW} Ratelimited {Fore.WHITE}- {Fore.YELLOW}Waiting {Fore.WHITE}1 {Fore.YELLOW}minute')
-                                time.sleep(60)
-                                continue
-                        break
+                        if decline == True:
+                            while True:
+                                r = req.post('https://auth.roblox.com/v1/logout')
+                                csrf = r.headers['X-CSRF-TOKEN']
+                                headers = {
+                                    'x-csrf-token': csrf
+                                    }
+                                a = req.post(f'https://trades.roblox.com/v1/trades/{trade}/decline', headers=headers).text
+                                if a == '{}':
+                                    print(f'{Fore.WHITE}[{Fore.RED}x{Fore.WHITE}] {Fore.RED}Trade Declined {Fore.WHITE}- {Fore.RED}{me} {Fore.WHITE}for {Fore.RED}{them}')
+                                    bad += 1
+                                    break
+                                elif 'Declined' in a:
+                                    break
+                                else:
+                                    print(f'{Fore.WHITE}[{Fore.YELLOW}-{Fore.WHITE}]{Fore.YELLOW} Ratelimited {Fore.WHITE}- {Fore.YELLOW}Waiting {Fore.WHITE}1 {Fore.YELLOW}minute')
+                                    time.sleep(60)
+                                    continue
+                            with open('checked.txt','a') as p:
+                                p.writelines(f'{trade_identifier}\n')
+                            break
 
-                    elif decline == False:
-                        me_hook = '\n'.join(me_hook)
-                        them_hook = '\n'.join(them_hook)
+                        elif decline == False:
+                            me_hook = '\n'.join(me_hook)
+                            them_hook = '\n'.join(them_hook)
 
-                        profit = int(them) - int(me)
-                        percentage = (1 - int(me) / int(them)) * 100
-                        if '.' in str(percentage):
-                            if len(str(percentage).split('.')[1]) >= 3:
-                                percentage = round(percentage, 2)
+                            profit = int(them) - int(me)
+                            percentage = (1 - int(me) / int(them)) * 100
+                            if '.' in str(percentage):
+                                if len(str(percentage).split('.')[1]) >= 3:
+                                    percentage = round(percentage, 2)
 
-                        if me_proj == True:
-                            print(f'{Fore.WHITE}[{Fore.LIGHTGREEN_EX}+{Fore.WHITE}] {Fore.LIGHTGREEN_EX}Giving Projected Trade Found {Fore.WHITE}- {Fore.LIGHTGREEN_EX}{trade} {Fore.WHITE}- {Fore.LIGHTGREEN_EX}{me} {Fore.WHITE}for {Fore.LIGHTGREEN_EX}{them}')
-                        elif me_proj == False:
-                            print(f'{Fore.WHITE}[{Fore.LIGHTGREEN_EX}+{Fore.WHITE}] {Fore.LIGHTGREEN_EX}Good Trade Found {Fore.WHITE}- {Fore.LIGHTGREEN_EX}{trade} {Fore.WHITE}- {Fore.LIGHTGREEN_EX}{me} {Fore.WHITE}for {Fore.LIGHTGREEN_EX}{them}')
-                        good += 1
-                        if trade_mode == 'inbound':
-                            data = {
-                                'embeds':[{
-                                    'color': int('880808',16),
-                                    'fields': [
-                                        {'name': f'📤 Giving [{me}]','value': f'{me_hook}','inline':True},
-                                        {'name': f'\u200b','value': f'\u200b','inline':True},
-                                        {'name': f'📥 Receiving Items: [{them}]','value': f'{them_hook}','inline':True},
-                                        {'name': '💸 Profit','value': f'{profit} ({percentage}%)','inline':False},
-                                        ]
-                                    }]
-                                }
-                            if not f'{trade_identifier}' in dont:
-                                requests.post(webhook, json=data)
-                                with open('dontsend.txt','a') as p:
-                                    p.writelines(f'{trade_identifier}\n')
+                            if me_proj == True:
+                                print(f'{Fore.WHITE}[{Fore.LIGHTGREEN_EX}+{Fore.WHITE}] {Fore.LIGHTGREEN_EX}Giving Projected Trade Found {Fore.WHITE}- {Fore.LIGHTGREEN_EX}{trade} {Fore.WHITE}- {Fore.LIGHTGREEN_EX}{me} {Fore.WHITE}for {Fore.LIGHTGREEN_EX}{them}')
+                            elif me_proj == False:
+                                print(f'{Fore.WHITE}[{Fore.LIGHTGREEN_EX}+{Fore.WHITE}] {Fore.LIGHTGREEN_EX}Good Trade Found {Fore.WHITE}- {Fore.LIGHTGREEN_EX}{trade} {Fore.WHITE}- {Fore.LIGHTGREEN_EX}{me} {Fore.WHITE}for {Fore.LIGHTGREEN_EX}{them}')
+                            good += 1
+                            if trade_mode == 'inbound':
+                                data = {
+                                    'embeds':[{
+                                        'color': int('880808',16),
+                                        'fields': [
+                                            {'name': f'📤 Giving [{me}]','value': f'{me_hook}','inline':True},
+                                            {'name': f'\u200b','value': f'\u200b','inline':True},
+                                            {'name': f'📥 Receiving Items: [{them}]','value': f'{them_hook}','inline':True},
+                                            {'name': '💸 Profit','value': f'{profit} ({percentage}%)','inline':False},
+                                            ]
+                                        }]
+                                    }
+                                if int(them - me) >= minimum_rap_for_webhook:
+                                    requests.post(webhook, json=data)
+                            with open('checked.txt','a') as p:
+                                p.writelines(f'{trade_identifier}\n')
+                            break
+                    else:
+                        skipped += 1
                         break
                 else:
                     print(f'{Fore.WHITE}[{Fore.YELLOW}-{Fore.WHITE}]{Fore.YELLOW} Ratelimited {Fore.WHITE}- {Fore.YELLOW}Waiting {Fore.WHITE}1 {Fore.YELLOW}minute')
@@ -210,4 +213,8 @@ Thread(target=title).start()
 values = rolimons()
 inbounds = scrape_trades()
 
-check(inbounds)
+if sort_by == 'ascending':
+    inbounds = inbounds[::-1]
+    check(inbounds)
+elif sort_by == 'descending':
+    check(inbounds)
